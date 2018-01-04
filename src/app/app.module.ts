c6aa2cc4ee373d9718a97d0679bee7f731ee413e
jared <jared.jacobson.2000@gmail.com>
2018-01-04 08:15:19 -0700

    removed test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
-import { TestComponent1Component } from './test-component1/test-component1.component';
 import { CharacterListComponent } from './character-list/character-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent,
-    TestComponent1Component,
     CharacterListComponent
   ],
   imports: [
