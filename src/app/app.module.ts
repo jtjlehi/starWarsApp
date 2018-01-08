afe9eca32940b68e8d646048f68b21e4012d2416
jared <jared.jacobson.2000@gmail.com>
2018-01-08 09:01:53 -0700

    fixed a few import bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { CharacterListComponent } from '../character-list/character-list.component';
+import { AppComponent } from './appComponent/app.component';
+import { CharacterListComponent } from './character-list/character-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import { CharacterListFilterPipe } from '../character-list/character-list-filter.pipe';
+import { CharacterListFilterPipe } from './character-list/character-list-filter.pipe';
 import { FormsModule } from '@angular/forms';
 import { ForceComponent } from './force/force.component';
 
